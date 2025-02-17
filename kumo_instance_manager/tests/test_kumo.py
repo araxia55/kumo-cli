9c8b1dbd987299890093a74dc627853ba29d8f90
araxia555 <raymund.herradura.nz@gmail.com>
2025-02-18 11:20:09 +1300

    fix default region

--- kumo_instance_manager/tests/test_kumo.py
+++ kumo_instance_manager/tests/test_kumo.py
@@ -1,8 +1,9 @@
-import sys
 import os
+import sys
 import pytest
 from datetime import datetime, timezone
 from moto import mock_ec2
+import boto3
 from kumo_instance_manager.utils import initialize_boto3_client
 
 # Add the project directory to the Python path
@@ -11,8 +12,15 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 # Now you can import the list_instance function
 from kumo_instance_manager.kumo import list_instance
 
+# Mocking AWS EC2 service
 @mock_ec2
 def test_list_instance_structure():
+    # Set environment variables for AWS credentials and region
+    os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
+    os.environ['AWS_REGION'] = 'us-east-1'
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    
     # Set up mock EC2 client and create instances
     ec2 = initialize_boto3_client('ec2', region='us-east-1')
     ec2.run_instances(
